b4145b942906aa25efd93d46b442860bab238c00
cibo-24 <cibo.kenan@gmail.com>
2024-01-31 22:21:17 +0300

    Math-Functions

---
+++ Python-Developer/02-Python-Basics/01-data-types.py
@@ -0,0 +1,43 @@
+# Fundamental Data Types
+
+int 
+float
+bool
+str 
+list
+tuple
+set
+dict
+
+# Classes -> custom types
+
+# Specialized Data Types
+
+None # değer yok = 0
+
+# integer - tam sayı
+
+print(type(2+4))
+print(2-4)
+print(2*4)
+print(1)
+
+# float - ondalıklı sayı
+("float, bellekte bir integer sayıdan daha çok yer kaplar.")
+
+print(type(2/4))
+print(type(0.000001))
+
+# ** -> üs alma ve // -> kalanı verme işlemi
+
+("2'nin 4. kuvvetini alır.")
+print(2 ** 4)
+
+("bölme işleminin sonucu bir ondalık sayı olacak ise, sadece tam kısmını verir. Ondalık kısmını ihmal eder.")
+print(2 // 4)
+
+# % -> modular formülü, bölme işlemi yaparak sadece kalanı verir.
+
+print(5 % 4) # kalan : 1
+
+
---
+++ Python-Developer/02-Python-Basics/02-Math-Functions.py
@@ -0,0 +1,7 @@
+# Math Functions
+
+("round func, ondalıklı sayıyı en yakın tam sayıya yuvarlar")
+print(round(3.7))
+
+("sayının mutlak değerini alır")
+print(abs(-20))
